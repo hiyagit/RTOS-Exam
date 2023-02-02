602a2749ba4f8f9f36b1d62f5b6d9ce866a24c38
hiyagit <hiya131pnd@gmail.com>
2023-02-02 13:03:14 +0530

    Question 2: One Shot Timer

---
+++ q2_one_shot/build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "/home/training/esp/esp-idf/components/bootloader/subproject"
+  "/home/training/Desktop/rtos_hiya_02/q2_one_shot/build/bootloader"
+  "/home/training/Desktop/rtos_hiya_02/q2_one_shot/build/bootloader-prefix"
+  "/home/training/Desktop/rtos_hiya_02/q2_one_shot/build/bootloader-prefix/tmp"
+  "/home/training/Desktop/rtos_hiya_02/q2_one_shot/build/bootloader-prefix/src/bootloader-stamp"
+  "/home/training/Desktop/rtos_hiya_02/q2_one_shot/build/bootloader-prefix/src"
+  "/home/training/Desktop/rtos_hiya_02/q2_one_shot/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "/home/training/Desktop/rtos_hiya_02/q2_one_shot/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "/home/training/Desktop/rtos_hiya_02/q2_one_shot/build/bootloader-prefix/src/bootloader-stamp${cfgdir}") # cfgdir has leading slash
+endif()
---
+++ q2_one_shot/main/q2_one_shot.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/timers.h"
+
+TaskHandle_t task1_handle, task2_handle, task3_handle;
+TimerHandle_t one_shot_handle;
+
+void task1(void *data)
+{
+    while(1)
+    {
+        printf(" Task 1\n");
+        vTaskDelay(1000);
+    }
+    vTaskDelete(task1_handle);
+}
+
+void task2(void *data)
+{
+    while(1)
+    {
+        printf(" Task 2\n");
+        vTaskDelay(2000);
+    }
+    vTaskDelete(task2_handle);
+}
+
+void timer_callback(TimerHandle_t one_shot_handle)
+{
+    printf("Timer Callback function: Timer triggered...\n");
+}
+
+void task3(void *data)
+{
+	while(1)
+	{
+		printf(" Task 3\n");
+
+        one_shot_handle = xTimerCreate("One_Shot_Timer",pdMS_TO_TICKS(10000),pdFALSE, NULL, timer_callback);
+        
+        if(pdPASS == (xTimerStart(one_shot_handle,5000)))
+        {
+            printf("Timer Start command is successfull!!\n");
+        }
+        vTaskDelete(task3_handle);
+    }
+}
+
+void app_main(void)
+{
+    BaseType_t res_1, res_2, res_3;
+
+    res_3 = xTaskCreate(task3,"third task", 2048, NULL, 7, &task3_handle);
+    if(res_3 == pdPASS)
+    {
+        printf("    Task 3 created successfully!!\n");
+    }
+
+    res_2 = xTaskCreate(task2,"second task", 2048, NULL, 6, &task2_handle);
+    if(res_2 == pdPASS)
+    {
+        printf("    Task 2 created successfully!!\n");
+    }
+    
+    res_1 = xTaskCreate(task1,"first task", 2048, NULL, 5, &task1_handle);
+    if(res_1 == pdPASS)
+    {
+        printf("    Task 1 created successfully!!\n");
+    }
+}
